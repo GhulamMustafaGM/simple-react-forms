28f7d24a5b853bdd4b961435d2a19a30affa2f42
Ghulam Mustafa <mustafaji@gmail.com>
2021-12-25 11:19:45 +0100

    simple-react-forms

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './index';
+
+function App() {
+  return (
+    <div className="form-container">
+      <form className='register-form'>
+        <input
+          id='first-name'
+          className='form-field'
+          type="text"
+          placeholder='First Name'
+          name='firstName'
+        />
+      </form>
+      <input
+        id='last-name'
+        className='form-field'
+        type="text"
+        placeholder='Last Name'
+        name='lastName'
+      />
+      <input
+        id='email'
+        className='form-field'
+        type="text"
+        placeholder='Email'
+        name='email'
+      />
+      <button
+        className='form-field'
+        type='submit'>
+        Register
+      </button>
+    </div>
+  );
+}
+
+export default App;
